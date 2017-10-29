708a45da8ed9d297e0ecbcb42699768091a9bcfd
Sebastian Jensen <apps@tsjdev-apps.de>
2017-10-29 13:37:14 +0100

    - add first test

--- RandomUserSharp/RandomUserSharp.Tests/RandomUserClientTests.cs
+++ RandomUserSharp/RandomUserSharp.Tests/RandomUserClientTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace RandomUserSharp.Tests
@@ -6,9 +7,30 @@ namespace RandomUserSharp.Tests
     [TestClass]
     public class RandomUserClientTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private RandomUserClient _randomUserClient;
+
+        [TestInitialize]
+        public void Initialize()
         {
+            _randomUserClient = new RandomUserClient();
+        }
+
+        [TestCleanup]
+        public void Cleanupo()
+        {
+            _randomUserClient.Dispose();
+            _randomUserClient = null;
+        }
+
+        [TestMethod]
+        public async Task GetRandomUserAsync()
+        {
+            var expectedResult = 1;
+            var count = 0;
+
+            var users = await _randomUserClient.GetRandomUsersAsync(count);
+
+            Assert.AreEqual(expectedResult, users.Count);
         }
     }
 }
