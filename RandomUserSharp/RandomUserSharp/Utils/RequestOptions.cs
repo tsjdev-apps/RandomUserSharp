47f92f6615e358522349c358324861abbf82ec3a
Sebastian Jensen <apps@tsjdev-apps.de>
2017-10-29 10:40:58 +0100

    - add requestoptions

---
+++ RandomUserSharp/RandomUserSharp/Utils/RequestOptions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RandomUserSharp.Models;
+
+namespace RandomUserSharp.Utils
+{
+    public class RequestOptions
+    {
+        public int Count { get; set; }
+        public Gender Gender { get; set; }
+        public string Seed { get; set; }
+        public bool UseLegoImages { get; set; }
+
+        public override string ToString()
+        {
+            var parameters = new Dictionary<string,string>();
+
+            if (Count > 1)
+                parameters.Add("results", Count < 5000 ? Count.ToString() : "5000");
+
+            if (Gender != Gender.Both)
+                parameters.Add("gender", Gender.ToString().ToLower());
+
+            if (!string.IsNullOrEmpty(Seed))
+                parameters.Add("seed", Seed);
+
+            var parameterString = string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+
+            if (UseLegoImages)
+                parameterString += "&lego";
+
+            return parameterString;
+        }
+    }
+}
