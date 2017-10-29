4d0a7d4c57002773eb62aaad6dbfd447274e1901
Sebastian Jensen <apps@tsjdev-apps.de>
2017-10-29 10:32:36 +0100

    - prepare randumuserclient

---
+++ RandomUserSharp/RandomUserSharp/RandomUserClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+
+namespace RandomUserSharp
+{
+    public class RandomUserClient : IDisposable
+    {
+        private readonly HttpClient _httpClient;
+
+        public RandomUserClient()
+        {
+            _httpClient = new HttpClient { BaseAddress = new Uri("http://api.randomuser.me/1.1") };
+        }
+
+        private bool _disposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            try
+            {
+                if (disposing)
+                    _httpClient.Dispose();
+            }
+            finally
+            {
+                _disposed = true;
+            }
+        }
+    }
+}
