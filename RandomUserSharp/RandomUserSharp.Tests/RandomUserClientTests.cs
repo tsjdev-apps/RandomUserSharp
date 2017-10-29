7fb1d86fdd5805db4cb547e7a97f5773e2663fd7
Sebastian Jensen <apps@tsjdev-apps.de>
2017-10-29 13:52:34 +0100

    - add more tests

--- RandomUserSharp/RandomUserSharp.Tests/RandomUserClientTests.cs
+++ RandomUserSharp/RandomUserSharp.Tests/RandomUserClientTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomUserSharp.Models;
 
 namespace RandomUserSharp.Tests
 {
@@ -23,14 +26,41 @@ namespace RandomUserSharp.Tests
         }
 
         [TestMethod]
-        public async Task GetRandomUserAsync()
+        public async Task GetRandomUserAsync_ResultCountIsEqualToExpectedResultCount()
         {
-            var expectedResult = 1;
-            var count = 0;
+            // arrange
+            const int firstExpectedResult = 1;
+            const int firstCount = 0;
+            const int secondExpectedResult = 1;
+            const int secondCount = 1;
+            const int thirdExpectedResult = 30;
+            const int thirdCount = 30;
+            
+            // act
+            var firstUsers = await _randomUserClient.GetRandomUsersAsync(firstCount);
+            var secondUsers = await _randomUserClient.GetRandomUsersAsync(secondCount);
+            var thirdUsers = await _randomUserClient.GetRandomUsersAsync(thirdCount);
 
-            var users = await _randomUserClient.GetRandomUsersAsync(count);
+            // assert
+            Assert.AreEqual(firstExpectedResult, firstUsers.Count);
+            Assert.AreEqual(secondExpectedResult, secondUsers.Count);
+            Assert.AreEqual(thirdExpectedResult, thirdUsers.Count);
+        }
 
-            Assert.AreEqual(expectedResult, users.Count);
+        [TestMethod]
+        public async void GetRandomUserAsync_UserResultTakeCareOfProvidedGender()
+        {
+            // act
+            var femaleUsers = await _randomUserClient.GetRandomUsersAsync(20, Gender.Female);
+            var maleUsers = await _randomUserClient.GetRandomUsersAsync(20, Gender.Male);            
+            var femaleGenders = femaleUsers.Select(u => u.Gender).Distinct().ToList();
+            var maleGenders = maleUsers.Select(u => u.Gender).Distinct().ToList();
+
+            // arrange
+            Assert.AreEqual(1, femaleGenders.Count);
+            Assert.AreEqual(1, maleGenders.Count);
+            Assert.AreEqual(Gender.Female, femaleGenders.Single());
+            Assert.AreEqual(Gender.Male, maleGenders.Single());
         }
     }
 }
