b5f3d93974d823f37f8156d4bf9f12fe0d19c2b2
Sebastian Jensen <apps@tsjdev-apps.de>
2018-07-03 07:50:56 +0200

    - add timezone model

---
+++ RandomUserSharp/RandomUserSharp/Models/Timezone.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace RandomUserSharp.Models
+{
+    public class Timezone
+    {
+        [JsonProperty("offset")]
+        public string Offset { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
